cfb6a17f567cc7eac4ec66d46bcdf0d37ae4ad5e
hatlonely <hatlonely@gmail.com>
2016-05-08 22:41:59 +0800

    add register.blade

--- inp/app/Http/routes.php
+++ inp/app/Http/routes.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('register', function () {
+    return view('register');
+});
+
+Route::get('bar', function () {
+    return 'Hello World';
+});
+
+Route::get('user/{id}', function($id)
+{
+    return 'User '.$id;
+});
