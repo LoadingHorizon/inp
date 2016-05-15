1f48b0c6a923c05e06d06a35227a999cbc82159e
PlayJokes <panjie1992@gmail.com>
2016-05-15 21:33:58 +0800

    revert ‘revert’

---
+++ inp/resources/views/register.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>register</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+    <div class="container">
+        <div class="content">
+            <div class="title">hello world</div>
+        </div>
+    </div>
+
+</body>
+</html>
